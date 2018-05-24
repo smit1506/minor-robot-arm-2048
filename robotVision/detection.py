1c881186bbc7f59b97dabe85d59470b6c6a6b6e4
Frank Keuning <Frank_keuning@hotmail.com>
2018-05-24 12:43:43 +0200

    vision updates

--- robotVision/detection.py
+++ robotVision/detection.py
@@ -4,13 +4,15 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_rgb = cv2.imread('test5.jpg')
+img_rgb = cv2.imread('test9.jpg')
 img_gray = img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('template_2_big.png',0)
-w, h = template.shape[::-1]
+template = cv2.imread('templates/template_2_big.png',0)
+template2 = cv2.imread('templates/template_4_big.png',0)
+template3 = cv2.imread('templates/template_field.png',0)
 
 
-def getMatches(template):
+def getMatches(template,color):
+    w, h = template.shape[::-1]
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
     loc = np.where(res >= threshold)
@@ -24,8 +26,13 @@ def getMatches(template):
         if is_invalid_match:
             continue
         print(pt)
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), color , 2)
         last_pts.append(pt)
-        cv2.imwrite('res.png',img_rgb)
 
-getMatches(template)
+
+getMatches(template,(0,255,0))
+getMatches(template2,(255,0,0))
+getMatches(template3,(0,0,255))
+
+
+cv2.imwrite('res.png',img_rgb)
