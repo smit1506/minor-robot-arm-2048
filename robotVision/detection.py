fb3462bfd649e22c9656ca1a27e4ec36005c92ad
Ruben <smit1506@student.nhl.nl>
2018-05-24 12:13:40 +0200

    Vision tests and changes

--- robotVision/detection.py
+++ robotVision/detection.py
@@ -4,35 +4,28 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_rgb = cv2.imread('test.jpg')
+img_rgb = cv2.imread('test5.jpg')
 img_gray = img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('template_empty.png',0)
-template2 = cv2.imread('template_2.png',0)
-w, h = template2.shape[::-1]
+template = cv2.imread('template_2_big.png',0)
+w, h = template.shape[::-1]
 
 
-value = 0
+def getMatches(template):
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+    last_pts = []
 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-threshold = 1
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    value += 1
-    print(value)
+    for pt in zip(*loc[::-1]):
+        is_invalid_match = False
+        for last_pt in last_pts:
+            if (last_pt) and abs(pt[0] - last_pt[0]) <= 10 and abs(pt[1] - last_pt[1]) <= 10:
+                is_invalid_match = True
+        if is_invalid_match:
+            continue
+        print(pt)
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
+        last_pts.append(pt)
+        cv2.imwrite('res.png',img_rgb)
 
-value = 0
-
-res = cv2.matchTemplate(img_gray,template2,cv2.TM_CCOEFF_NORMED)
-threshold = 0.97
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
-    value += 1
-    print(value)
-
-cv2.imwrite('res.png',img_rgb)
-cv2.imwrite('t2.png',template2)
-cv2.imwrite('imgGray.png',img_gray)
-
-
+getMatches(template)
