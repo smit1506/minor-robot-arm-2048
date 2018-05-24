87899b7764a38ef50b4422d8c7938b2cdf346bab
Frank Keuning <Frank_keuning@hotmail.com>
2018-05-24 13:32:18 +0200

    vision playgrid added

--- robotVision/detection.py
+++ robotVision/detection.py
@@ -9,6 +9,10 @@ img_gray = img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 template = cv2.imread('templates/template_2_big.png',0)
 template2 = cv2.imread('templates/template_4_big.png',0)
 template3 = cv2.imread('templates/template_field.png',0)
+template4 = cv2.imread('templates/template_8_big.png',0)
+template5 = cv2.imread('templates/template_16_big.png',0)
+
+
 
 
 def getMatches(template,color):
@@ -28,11 +32,34 @@ def getMatches(template,color):
         print(pt)
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), color , 2)
         last_pts.append(pt)
+        
+
+def getField(fieldTemplate):
+    blockWidth, blockHeigth = fieldTemplate.shape[::-1]
+    blockWidth /= 4
+    blockHeigth /= 4
+    res = cv2.matchTemplate(img_gray,fieldTemplate,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where(res >= threshold)
+
+    pt = zip(*loc[::-1])[0]
+
+    print(blockWidth, blockHeigth)
+    for x in range(1,5):
+        for y in range (1,5):
+            cv2.rectangle(img_rgb , pt , (pt[0] + (blockWidth * x)  , pt[1] + (blockHeigth * y)), (125,125,125) , 2)
 
 
+        
+
+
+
+
+getField(template3)
 getMatches(template,(0,255,0))
 getMatches(template2,(255,0,0))
 getMatches(template3,(0,0,255))
-
+getMatches(template4,(125,0,125))
+getMatches(template5,(0,125,125))
 
 cv2.imwrite('res.png',img_rgb)
