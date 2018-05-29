a6b8f0ddbd4f8b0867bc489baca51731dab93260
Frank Keuning <Frank_keuning@hotmail.com>
2018-05-29 11:51:40 +0200

    .

--- robotVision/test.py
+++ robotVision/test.py
@@ -1,13 +1,95 @@
-import cv2
+# # import the necessary packages
+# import numpy as np
+# import argparse
+# import glob
+# import cv2
+#
+# def auto_canny(image, sigma=0.33):
+# 	# compute the median of the single channel pixel intensities
+# 	v = np.median(image)
+#
+# 	# apply automatic Canny edge detection using the computed median
+# 	lower = int(max(0, (1.0 - sigma) * v))
+# 	upper = int(min(255, (1.0 + sigma) * v))
+# 	edged = cv2.Canny(image, lower, upper)
+#
+# 	# return the edged image
+# 	return edged
+#
+#
+#
+# # loop over the images
+#
+# 	# load the image, convert it to grayscale, and blur it slightly
+# image = cv2.imread("test11.jpg")
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+#
+# 	# apply Canny edge detection using a wide threshold, tight
+# 	# threshold, and automatically determined threshold
+# wide = cv2.Canny(blurred, 10, 200)
+# tight = cv2.Canny(blurred, 225, 250)
+# auto = auto_canny(blurred)
+#
+# 	# show the images
+# cv2.imshow("Original", image)
+# cv2.imshow("Wide", wide)
+# cv2.imshow("tight", tight)
+# cv2.imshow("Auto", auto)
+# cv2.waitKey(0)
+
 import numpy as np
+import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('test5.jpg',0)
-edges = cv2.Canny(img,100,200)
+MIN_MATCH_COUNT = 1
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+img1 = cv2.imread('template_2_big.png',0)          # queryImage
+img2 = cv2.imread('test9.jpg',0) # trainImage
 
-plt.show()
+# Initiate SIFT detector
+sift = cv2.SIFT()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+else:
+    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
