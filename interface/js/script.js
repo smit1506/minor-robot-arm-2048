36a13649a5b8ed417c088fc5f32b820f40a4d853
Ruben <smit1506@student.nhl.nl>
2018-06-28 13:34:44 +0200

    Added bootstrap, added front-end states

--- interface/js/script.js
+++ interface/js/script.js
@@ -1,5 +1,17 @@
+function calibrate(origin){
+    console.log("click");
+    hide(origin);
+    document.getElementById('calibration-menu').classList.remove('hidden');
+}
+function finishCalibration() {
+    document.getElementById('calibration-menu').classList.add('hidden');
+    document.getElementById('start-button').classList.remove('hidden');
+}
 function activate(origin) {
-    const parent = origin.parentNode;
-    parent.removeChild(origin);
+    hide(origin);
     document.getElementById('canvas').classList.remove('hidden');
+}
+
+function hide(element) {
+    element.parentNode.removeChild(element);
 }
