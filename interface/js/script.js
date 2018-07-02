7709bf3d3344e6378f4f57316eabcf89929addca
Ruben <smit1506@student.nhl.nl>
2018-07-02 14:28:05 +0200

    Moved interface back to main directory

--- interface/js/script.js
+++ interface/js/script.js
@@ -1,5 +1,4 @@
 function calibrate(origin){
-    console.log("click");
     hide(origin);
     document.getElementById('calibration-menu').classList.remove('hidden');
 }
@@ -10,6 +9,13 @@ function finishCalibration() {
 function activate(origin) {
     hide(origin);
     document.getElementById('canvas').classList.remove('hidden');
+    fetch('http://localhost:8080/run').then(function(response) {
+      return response.text();
+    }).then(function(data) {
+      console.log(data);
+    }).catch(function(error) {
+      console.log(error)
+    });
 }
 
 function hide(element) {
